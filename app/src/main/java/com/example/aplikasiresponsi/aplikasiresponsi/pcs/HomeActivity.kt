e9474f9191180cc9008a3f9ab04db8ecddfd2bf6
Ariflutfi <arif.kurniawan@students.amikom.ac.id>
2023-01-10 17:07:18 +0700

    first commit

---
+++ app/src/main/java/com/example/aplikasiresponsi/aplikasiresponsi/pcs/HomeActivity.kt
@@ -0,0 +1,4 @@
+package com.example.aplikasiresponsi.aplikasiresponsi.pcs
+
+class HomeActivity {
+}
---
+++ app/src/main/java/com/example/aplikasiresponsi/aplikasiresponsi/pcs/RecoveryActivity.kt
@@ -0,0 +1,11 @@
+package com.example.aplikasiresponsi.aplikasiresponsi.pcs
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RecoveryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recovery)
+    }
+}
